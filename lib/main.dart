81984d712bef577bdab6a74f1e319fe32827621b
JESUS_ADRIAN_ESQUIVEL_MARTINEZ <a.22308051281176@cbtis128.edu.mx>
2025-02-27 03:57:48 +0000

    finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,70 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisIconos());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisIconos extends StatelessWidget {
+  const MisIconos({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Jesus Adrian"),
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+          centerTitle: true,
+          backgroundColor: Color(0xff2766ef),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  "Mat:22308051281176",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.normal),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _buildIconWithSubtitle(
+                      Icons.account_circle, 'Inicio', Color(0xff23c7cd), 30),
+                  _buildIconWithSubtitle(
+                      Icons.wallet, 'Cartera', Color(0xff95560b), 30),
+                  _buildIconWithSubtitle(
+                      Icons.receipt, 'Recibo', Color(0xff7c0c69), 30),
+                ],
+              ),
+              SizedBox(height: 20), // Espacio entre las filas
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  _buildIconWithSubtitle(
+                      Icons.access_alarm, 'Alarma', Color(0xffb50e0e), 30),
+                  _buildIconWithSubtitle(Icons.navigation_sharp, 'Navegacion',
+                      Color(0xff3b0998), 30),
+                  _buildIconWithSubtitle(
+                      Icons.table_restaurant, 'Meza', Color(0xdf0d9724), 30),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget _buildIconWithSubtitle(
+      IconData iconData, String subtitle, Color color, double size) {
+    return Column(
+      children: <Widget>[
+        Icon(iconData, color: color, size: size),
+        SizedBox(height: 10),
+        Text(subtitle),
+      ],
     );
   }
 }
